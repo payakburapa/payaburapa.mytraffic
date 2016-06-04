87c864bd01f6a90c7fed114fdd0cb2646be9fbca
New <payakburapa@gmail.com>
2016-06-04 14:06:14 -0700

    Start App(Comment)

--- app/src/main/java/luckyhuman/payakburapa/mytraffic/MainActivity.java
+++ app/src/main/java/luckyhuman/payakburapa/mytraffic/MainActivity.java
@@ -3,12 +3,12 @@ package luckyhuman.payakburapa.mytraffic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {// Start Main Class (เริ่ม Code หลัก)
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {// Start Main Method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-}
+    }// End Main Method
+}// End Main Class (จบ Code หลัก)
